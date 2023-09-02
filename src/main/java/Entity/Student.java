205f1656f408aeb0034fc33beb0c289816fb8bc8
apoorvDwivedi1996 <apoorv.dwivedi.mailbox@gmail.com>
2023-09-02 12:53:37 -0400

    version 2 (added  getter and setter methond to student class)

--- src/main/java/Entity/Student.java
+++ src/main/java/Entity/Student.java
@@ -8,4 +8,43 @@ public class Student {
     private  int rank;
     private String classTeacher;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStandard() {
+        return standard;
+    }
+
+    public void setStandard(int standard) {
+        this.standard = standard;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    public String getClassTeacher() {
+        return classTeacher;
+    }
+
+    public void setClassTeacher(String classTeacher) {
+        this.classTeacher = classTeacher;
+    }
 }
