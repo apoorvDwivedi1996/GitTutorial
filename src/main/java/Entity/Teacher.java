dfad76c7f22acc0423e3fb7c148931cf7b98fd46
apoorvDwivedi1996 <apoorv.dwivedi.mailbox@gmail.com>
2023-09-02 12:54:54 -0400

    version 3 (added  getter and setter methond to teacher class)

--- src/main/java/Entity/Teacher.java
+++ src/main/java/Entity/Teacher.java
@@ -5,4 +5,28 @@ public class Teacher {
     private String name;
     private String subject;
     private int classNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public int getClassNumber() {
+        return classNumber;
+    }
+
+    public void setClassNumber(int classNumber) {
+        this.classNumber = classNumber;
+    }
 }
